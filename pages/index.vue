f92ad30bf8b07cf1b44729b8cf0fc0dbb7493ee4
Pietro Stivanello <68153217+pedrito-96@users.noreply.github.com>
2023-06-08 09:22:43 +0200

    commented

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+// import { gsap } from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-const green = ref(null);
+// const green = ref(null);
 
-if (process.client) {
-gsap.registerPlugin(ScrollTrigger);
-}
+// if (process.client) {
+// gsap.registerPlugin(ScrollTrigger);
+// }
 
-onMounted(() => {
-  gsap.to(green.value, {
-    scrollTrigger: {
-      trigger: green.value,
-      start: "top 170px",
-      end: "top top",
-      toggleActions: "restart null null null",
-      // markers: true,
-    },
-    opacity: 0.1,
-    y: -100,
-    duration: 5,
+// onMounted(() => {
+//   gsap.to(green.value, {
+//     scrollTrigger: {
+//       trigger: green.value,
+//       start: "top 170px",
+//       end: "top top",
+//       toggleActions: "restart null null null",
+//       markers: true,
+//     },
+//     opacity: 0.1,
+//     y: -100,
+//     duration: 5,
   
-  }
-  );
-  // gsap.to(green.value, {rotation: 360, x:500, duration: 10});
-})
+//   }
+//   );
+//   // gsap.to(green.value, {rotation: 360, x:500, duration: 10});
+// })
 
 </script>
 
@@ -32,7 +32,7 @@ onMounted(() => {
 <template>
 <LogoRockAShaka></LogoRockAShaka>    
 
-<!-- <div  class=" w-32 h-32 bg-slate-500 mt-[1000px]">
+<!-- <div ref="green" class=" w-32 h-32 bg-slate-500 mt-[1000px]">
 S
 </div> -->
 
